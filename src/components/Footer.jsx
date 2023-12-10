c117b4f0a269c4dd3e76f4482cc8bd532cc7928d
Pepe Checa <chekchek@outlook.esgit config --global user.email chekchek@outlook.esgit config --global user.email chekchek@outlook.es>
2023-12-10 22:06:53 +0100

    Cambios en el footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { linkedin } from '../assets';
 const Footer = () => {
   return (
     <footer className="bg-black text-white py-4 flex items-end justify-end">
-      <a href="https://www.linkedin.com/tu_perfil" target="_blank" rel="noopener noreferrer" className="mr-2">
+      <a href="https://www.linkedin.com/in/pepecheca" target="_blank" rel="noopener noreferrer" className="mr-2">
         <img className="w-5 h-5 sm:w-6 sm:h-6 md:w-6 md:h-6 lg:w-6 lg:h-6 xl:w-6 xl:h-6 object-contain" src={linkedin} alt="LinkedIn" />
       </a>
       <p className='mr-6'>chekchek@outlook.es | chek183chek@gmail.com</p>
